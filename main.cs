438df9b2a9920796c724e2c2e9bb03768c3793e5
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-05-10 14:50:42 +0000

    .

--- main.cs
+++ main.cs
@@ -25,6 +25,45 @@ public class Book
     Title = title;
     Author = author;
     PublicationYear = publicationYear;
-    
+    isAvailable = true;
+    numberOfPages = 0;
+    genre = "Unknown";
   }
+
+  public Book(string title, string author, int publicationYear, bool isAvailable, int numberOfPages, string genre)
+  {
+      Title = title;
+      Author = author;
+      PublicationYear = publicationYear;
+      this.isAvailable = isAvailable;
+      this.numberOfPages = numberOfPages;
+      this.genre = genre;
+  }
+
+  public void BorrowBook()
+      {
+          if (isAvailable)
+          {
+              isAvailable = false;
+              Console.WriteLine("You've borrowed the book. Please return it on time.");
+          }
+          else
+          {
+              Console.WriteLine("Sorry, the book is currently not available.");
+          }
+      }
+
+      public void ReturnBook()
+      {
+          isAvailable = true;
+          Console.WriteLine("Thank you for returning the book.");
+      }
+
+      public void DisplayBookInfo()
+      {
+          Console.WriteLine($"Title: {Title}, Author: {Author}, Publication Year: {PublicationYear}");
+      }
+  }
+
+  
   
