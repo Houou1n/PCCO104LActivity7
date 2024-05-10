367910c4380c3f37dedea2d5e50defc09890511d
Houou1n <158658886+Houou1n@users.noreply.github.com>
2024-05-10 14:22:30 +0000

    wotah break

--- main.cs
+++ main.cs
@@ -2,5 +2,29 @@ using System;
 
 public class Book
 {
-  public string 
+  public string Title { get; set; }
+  public string Author { get; set; }
+  public int PublicationYear { get; set; }
+
+  private bool isAvailable;
+  private int numberOfPages;
+  private string genre;
+
+  public book()
+  {
+    Title = "Unknown";
+    Author = "Unknown";
+    PublicationYear = 0;
+    isAvailable = true;
+    numberOfPages = 0;
+    genre = "Unknown";
+  }
+
+  public Book(string title, string author, int publicationYear)
+  {
+    Title = title;
+    Author = author;
+    PublicationYear = publicationYear;
+    
+  }
   
